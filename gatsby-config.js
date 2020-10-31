57a5911e02fc0dca6c5886e1d601574fa4685367
dekaio <47273651+dekaio@users.noreply.github.com>
2020-10-31 15:15:43 +0530

    filesready

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `content`,
+        path: `${__dirname}/src/content`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -27,6 +27,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/content/details.md
@@ -0,0 +1,8 @@
+---
+greetings: "Hi there!"
+emoji: "👋"
+title: "I am Devika"
+subtitlePrefix: "I am an embedded software Engineer"
+subtitleHighlight: "I am a cool person"
+---
+Embedded Software Engineer and Freelance Web Developer. Based in Munich.
